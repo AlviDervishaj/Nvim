42eba3590790f1add26a17cd3b46eb68d9dbf3f3
Alvi Dervishaj <alvidervishaj9@gmail.com>
2023-07-25 22:03:06 +0200

    Working neovim config with rose-pine colorscheme

---
+++ lua/alvi/packer.lua
@@ -0,0 +1,43 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer cna manage itself
+  use 'wbthomason/packer.nvim'
+  -- Telescope
+  use {
+	  'nvim-telescope/telescope.nvim', tag='0.1.0',
+	  requires = {{'nvim-lua/plenary.nvim'}}
+  }
+  -- Rose Pine Colorscheme
+  use {
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		vim.cmd('colorscheme rose-pine')
+	  end
+  }
+  -- Treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  -- Harpoon
+  use 'theprimeagen/harpoon'
+  -- Undotree
+  use 'mbbill/undotree'
+  -- Vim fugitive
+  use 'tpope/vim-fugitive'
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'dev-v3',
+    requires = {
+      --- Uncomment these if you want to manage LSP servers from neovim
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+}
+end)
