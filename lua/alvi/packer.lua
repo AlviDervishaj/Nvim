0701b17e1f4f7c896451d170d3c5a8e7978561bb
Alvi Dervishaj <alvidervishaj9@gmail.com>
2023-08-30 22:48:52 +0200

    Updated config to include flutter lsp

--- lua/alvi/packer.lua
+++ lua/alvi/packer.lua
@@ -5,19 +5,24 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   -- Telescope
   use {
-	  'nvim-telescope/telescope.nvim', tag='0.1.0',
-	  requires = {{'nvim-lua/plenary.nvim'}}
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use {
+    'akinsho/flutter-tools.nvim',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   -- Rose Pine Colorscheme
   use {
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		vim.cmd('colorscheme rose-pine')
-	  end
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
   }
   -- Treesitter
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   -- Harpoon
   use 'theprimeagen/harpoon'
   -- Undotree
@@ -29,15 +34,15 @@ return require('packer').startup(function(use)
     branch = 'dev-v3',
     requires = {
       --- Uncomment these if you want to manage LSP servers from neovim
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
+      { 'neovim/nvim-lspconfig' },
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     }
-}
+  }
 end)
